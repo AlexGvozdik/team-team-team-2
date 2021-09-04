1823bbae65ff515a16a6e8102569c35e614aa6c9
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-09-04 21:38:37 +0300

    Модалка
    Добавила модальное окно, пока без анимации и прочего

---
+++ src/js/modal-footer.js
@@ -0,0 +1,25 @@
+const refs = {
+    modalFooter:document.querySelector('.backdrop-dev'),
+    devLink:document.querySelector('.goit-link'),
+    btnClose:document.querySelector('.button-close')
+}
+
+refs.devLink.addEventListener('click', clickOnLink);
+refs.btnClose.addEventListener('click', clickOnBtnClose);
+document.addEventListener('keydown', clickOnEsc)
+
+function clickOnLink () {
+    refs.modalFooter.classList.remove('is-hidden')
+}
+
+function clickOnBtnClose(){
+    refs.modalFooter.classList.add('is-hidden')
+}
+
+function clickOnEsc(){
+    if (refs.modalFooter.classList.contains('is-hidden')){
+        return;
+    } else {
+        refs.modalFooter.classList.add('is-hidden')
+    }
+}
